4ea74bb4223c7abbb12331e1c22145198cd74dc3
thomas-yamada <the.important.work@gmail.com>
2025-05-29 23:15:31 -0700

    Update leading_fraction_from_polynomial.r
    fixed to calculate the constant properly

--- leading_fraction_from_polynomial.r
+++ leading_fraction_from_polynomial.r
@@ -1,19 +1,24 @@
 #input a polynomial with 10 or less terms
 INPUT_POLYNOMIAL := 
 
+#split apart the polynomial into coefficient/constant and variable/term
 is_match($c1:n$t1:term + $c2:n$t2:term + $c3:n$t3:term + $c4:n$t4:term + $c5:n$t5:term + $c6:n$t6:term + $c7:n$t7:term + $c8:n$t8:term + $c9:n$t9:term + $c10:n$t10:term, INPUT_POLYNOMIAL)
 #take each term and put them into a list
-term_list := [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10]
+term_list := [t1,t2,t3,t4,t5,t6,t7,t8,t9,t10]
+#take each coefficient/constant and put them into a list
+const_list := [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10]
 #set a math func for each term
 math_funcs(one_h, two_h, three_h, four_h, five_h, six_h, seven_h, eight_h, nine_h, ten_h)
 #setup a function to equal each term. if you have multiple possible variables that can be used for the question, put all of them in
 one_h(x, t) = t1; two_h(x, t) = t2; three_h(x, t) = t3; four_h(x, t) = t4; five_h(x, t) = t5; six_h(x, t) = t6; seven_h(x, t) = t7; eight_h(x, t) = t8; nine_h(x, t) = t9; ten_h(x, t) = t10
 #compute all of the functions of each term, putting "1" in for each possible variable.
 altered := [compute(one_h(1, 1)), compute(two_h(1,1)), compute(three_h(1,1)), compute(four_h(1,1)), compute(five_h(1,1)), compute(six_h(1,1)), compute(seven_h(1,1)), compute(eight_h(1,1)), compute(nine_h(1,1)), compute(ten_h(1,1))]
+#multiplies the coefficients/constants with the left behind numberator, pretty much only changing the constant, because thats the only one with a left behind numberator
+altered1_5 := list_transform(altered, simplify(nth_item(index, altered)*nth_item(index, const_list)))
 #remove "undefined"
-altered2 := list_filter(altered, not(is_match(:x, item)))
-#isolate variables by multiplying original terms by reciprocal of the coefficient
-isolate_var := [compute((t1)/(nth_item(1, altered2))), compute((t2)/(nth_item(2, altered2))), compute((t3)/(nth_item(3, altered2))), compute((t4)/(nth_item(4, altered2))), compute((t5)/(nth_item(5, altered2))), compute((t6)/(nth_item(6, altered2))), compute((t7)/(nth_item(7, altered2))), compute((t8)/(nth_item(8, altered2))), compute((t9)/(nth_item(9, altered2))), compute((t10)/(nth_item(10, altered2)))]
+altered2 := list_filter(altered1_5, not(is_match(:x, item)))
+#isolate variables by multiplying original terms by the origianl coefficient/constant, then multiply that by the reciprocal of the coefficient
+isolate_var := [compute((t1*c1)/(nth_item(1, altered2))), compute((t2*c2)/(nth_item(2, altered2))), compute((t3*c3)/(nth_item(3, altered2))), compute((t4*c4)/(nth_item(4, altered2))), compute((t5*c5)/(nth_item(5, altered2))), compute((t6*c6)/(nth_item(6, altered2))), compute((t7*c7)/(nth_item(7, altered2))), compute((t8*c8)/(nth_item(8, altered2))), compute((t9*c9)/(nth_item(9, altered2))), compute((t10*c10)/(nth_item(10, altered2)))]
 #remove 1
 isolate_var2 := list_filter(isolate_var, not(is_match(1, item)))
 #turn all variables into a square root so that all variables are caught by pattern matching
